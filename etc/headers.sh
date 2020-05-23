cd7673f3505cda1ae5c117068816e07081ed7435
Tim Menzies <tim.menzies@gmail.com>
2020-05-23 12:54:02 -0400

    saving

--- etc/headers.sh
+++ etc/headers.sh
@@ -7,15 +7,15 @@ i='https://i2.wp.com/auk-ward.co.uk/wp-content/uploads/2017/02/auk-ward-icon.png
 i="$r/etc/img/coins.png"
 
 cat <<EOF>/tmp/head$$
-<a name=top><img align=left width=400 src="$i">
-<h1 align=right><a href="$f/README.md#top">$t</a></h1> 
+<a name=top><img align=right width=400 src="$i">
+<h1 align=left><a href="$f/README.md#top">$t</a></h1> 
 <p align=right> <a
 href="$r/LICENSE.md#top">license</a> :: <a
 href="$r/INSTALL.md#top">install</a> :: <a
 href="$r/CODE_OF_CONDUCT.md#top">contribute</a> :: <a
 href="$u/issues">issues</a> :: <a
 href="$r/CITATION.md#top">cite</a> :: <a
-href="$r/CONTACT.md#top">contact</a> </p><p align=right> 
+href="$r/CONTACT.md#top">contact</a> </p><p align=left> 
 <img src="$b/license-mit-red">   
 <img src="$b/language-gawk-orange">    
 <img src="$b/purpose-ai,se-blueviolet">
