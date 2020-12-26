9d5a3296e3aeddcdb02434ecf5b735f2553cd308
Tim Menzies <timm@ieee.org>
2020-12-25 21:37:05 -0500

    save

--- src/code.lisp
+++ src/code.lisp
@@ -1,66 +1,53 @@
 ; vim: noai:ts=2:sw=2:et:
 
+"## SYNOPSIS
+
+Store data as table rows. Summarize
+columns into table headers
+
+## USAGE
+
+      (fromFile (tbl+) \"xxx.csv\")
+
+## LICENSE
+
+(c) 2020, MIT License, Tim Menzies, timm@ieee.org"
+
+
 (defklass col    (thing)    (pos 0) (txt "") (n 0) (w 1))
-(defklass sym    (col) seen (most 0) mode)
-(defklass sample (col) ok l (max (my sample max)) bins)
-(defklass skip   (col))
-(defklass row    (thing) (dom 0) cells)
-(defklass tbl    (thing) xs ys rows cols)
 
-;;;; tables
-(defun tbl+ (&optional lst &aux (tbl (make-instance 'tbl)))
-  (dolist (one lst tbl) (add tbl one)))
-
-(defmethod add ((i tbl) (r row)) (add i (? r cells)))
-(defmethod add ((i tbl) (lst cons)) 
-  (with-slots (cols rows) i
-    (if cols
-      (push (row+ lst i) rows)
-      (setf cols (cols+ i lst)))))
-
-(defmethod dom ((i tbl) &aux (s (make-instance 'sym)))
-  (dolist (row1 (? i rows))
-    (dotimes (n (my tbl samples))
-      (incf (dom row1 (if (one (? i rows)) 1 0))))
-    (add s (? row1 dom)))
-  (dolist (row1 (? i rows) (length (? i bins)))  
-    (setf (? row1 y) (bin s (? row1 dom)))))
-   
-(defmethod cols+ ((i tbl) lst &aux (n 0))
-  (labels ((what (txt) (if (has txt :less :more :num) 'sample 'sym)))
-    (mapcar #'(lambda (x) (col+ :isa (what x):pos (incf n) :txt x)) 
-            lst)))
-
-(defmethod fromDisc ((i tbl) file)
-  (with-csv (lst file) (add i lst)))
-
-;;;; rows
-(defun row+ (lst tbl &aux (r (make-instance 'row)))
-  (setf (? r cells) (mapcar #'add (? tbl cols) lst))
-  r)
-
-;;;; columns
 (defun col+ (&key (isa 'sym) (pos 0) (txt ""))
+  "Constructor for columns. Sets weights -1,1 if
+  `txt` contains '<' or '>' (respectively)."
   (let ((tmp (make-instance isa :txt txt :pos pos)))
     (setf  (? tmp w) (if  (has txt :less ) -1 1))
     tmp))
 
 (defmethod add ((i col) x)
+  "Genetic add to column. Ignore missing variables.
+   Else increment `n` and call `add1`."
   (unless (equal x "?")
     (incf (? i n))
     (add1 i x))
   x)
 
+(defmethod add1 ((i col) x ) 
+  "Default add1 is to do nothing."
+  x)
+
 (defmethod norm ((i col) x)
+  "If `x` is not missing, then call `norm1`."
   (if (equal x "?") x (norm1 i x)))
 
-(defmethod norm1 ((i col) x ) x)
+(defmethod norm1 ((i col) x ) 
+  "Default normalization is to do nothing." 
+  x)
 
-;;;; skipping
-(defmethod add1 ((i skip) x) x) 
+;;;;------------------------------------------------------------------
+(defklass sym    (col) seen (most 0) mode)
 
-;;;; symbol methods
 (defmethod add1 ((i sym) x)
+  "Update statistics on `x`."
   (with-slots (seen most mode) i
     (let* ((a   (assoc! x seen :if-needed 0))
            (now (incf (cdr  a))))
@@ -69,16 +56,21 @@
               mode x)))))
 
 (defmethod mix((i sym))
- (let ((ent 0))
-  (loop for (key . val) in (? i seen) do
-    (print `(key ,key val ,val))
-    (when (> val 0)
-      (let ((p (/ val (? i n))))
-       (decf ent (* p (log p 2))) )))
-  ent))
+  "return entropy"
+  (let ((ent 0))
+    (loop for (key . val) in (? i seen) do
+          (print `(key ,key val ,val))
+          (when (> val 0)
+            (let ((p (/ val (? i n))))
+              (decf ent (* p (log p 2))) )))
+    ent))
 
-;;;; sample methods
+;;;;------------------------------------------------------------------
+(defklass sample (col) ok l (max (my sample max)) bins)
+
 (defmethod add1 ((i sample) x)
+  "Update summary on numeric `x`. If contents full, then
+   replace anything old, at random, with `x`."
   (with-slots (ok l max) i
     (cond ((< (length l) max) 
            (push x l) 
@@ -87,25 +79,35 @@
            (setf (nth (randi (length l)) l) x) 
            (setf ok nil)))))
 
-; add optionals lo hi
-(defmethod mid  ((i sample)) (per i .5))
-(defmethod mix  ((i sample)) (/ (- (per i .9) (per i .1)) 2.56))
+(defmethod mid  ((i sample)) 
+  "Return median" 
+  (per i .5))
+
+(defmethod mix  ((i sample)) 
+  "Return standard deviation."
+  (/ (- (per i .9) (per i .1)) 2.56))
+
 (defmethod per ((i sample) &optional (p 0.5) (lo 0) (hi (length (? i l))))
   (nth (floor (+ lo (* p (- hi lo)))) (items i)))
 
 (defmethod items ((i sample))
+  "Return contents. Ensure they are sorted."
   (with-slots (ok l) i
     (unless ok
       (setf ok t)
       (sort (? i l) #'<))
     l))
 
-(defmethod norm1 ((i sample) x ) 
+(defmethod norm1 ((i sample) (x number)) 
+  "Return `x`, normalizes 0..1 for lo..hi."
   (min 1 (max 0 
               (/ (x - (per i 0) 
                     (- (per i 1) (per i 0) x) 1E-32)))))
 
 (defmethod split ((i sample))
+  "Divide contents into bins, of size at least sqrt(size(contents)).
+  Ignore iajdacent bins is they are too similar (median difference
+  less than 0.3*stdDeviation)."
   (let*((a   (items i))
         (all (length a))
         (eps (* (sd i) (my  sample cohen)))
@@ -129,8 +131,57 @@
                                 hi (+ hi n)))))))))
       (reverse (? i bins)))))
 
-(defmethod discretize ((i sample) x)
+(defmethod discretize ((i sample) (x number))
+  "Divide `x` into bins."
   (let ((out -1))
     (dolist (n (? i bins) (1+ out))
       (incf out)
       (if (< x n) (return-from discretize out)))))
+;;;;------------------------------------------------------------------
+(defklass skip   (col))
+
+;;;;------------------------------------------------------------------
+(defklass row    (thing) (dom 0) cells)
+
+(defun row+ (lst tbl &aux (r (make-instance 'row)))
+  "Constructor for rows. As a side-effect, update table
+  column summaries."
+  (setf (? r cells) (mapcar #'add (? tbl cols) lst))
+  r)
+
+
+;;;;------------------------------------------------------------------
+(defklass tbl    (thing) xs ys rows cols)
+
+(defun tbl+ (&optional rows &aux (tbl (make-instance 'tbl)))
+  "Constructor for tables. If given `rows` then add those in."
+  (dolist (row rows tbl) (add tbl row)))
+
+(defmethod add ((i tbl) (r row)) (add i (? r cells)))
+(defmethod add ((i tbl) (lst cons)) 
+  "Add one row to a table. If this is the first row, then
+  initialize the headers. Else add as data."
+  (with-slots (cols rows) i
+    (if cols
+      (push (row+ lst i) rows)
+      (setf cols (cols+ i lst)))))
+
+(defmethod dom ((i tbl) &aux (s (make-instance 'sym)))
+  "Compared to a random sample of other rows,
+   count how often one row dominates the others."
+  (dolist (row1 (? i rows))
+    (dotimes (n (my tbl samples))
+      (incf (dom row1 (if (one (? i rows)) 1 0))))
+    (add s (? row1 dom)))
+  (dolist (row1 (? i rows) (length (? i bins)))  
+    (setf (? row1 y) (bin s (? row1 dom)))))
+   
+(defmethod cols+ ((i tbl) lst &aux (n 0))
+  "Constructor for columns in a table."
+  (labels ((what (txt) (if (has txt :less :more :num) 'sample 'sym)))
+    (mapcar #'(lambda (x) (col+ :isa (what x):pos (incf n) :txt x)) 
+            lst)))
+
+(defmethod fromFile ((i tbl) file)
+  "Initialize table from disk."
+  (with-csv (lst file i) (add i lst)))
--- src/docs.lisp
+++ src/docs.lisp
@@ -1,28 +1,52 @@
-; vim: noai:ts=2:sw=2:et:
-(defun worker (mid top x)
-  "If a string, the print x. Else, if defklass,
-  start a new section. Else, for defun defmethod and
-  defmacro, print their doc string."
-  (typecase x
-    (string (format mid "~%~a~%" x))
-    (cons   (case (first x)
-              (defklass 
-                (format top "- ~(~S~) " (second x))
-                (format mid "~%## ~(~S~)~%~%" (second x)))
-              ((defun defmethod defmacro)
-               (when (stringp (fourth x))
-                 (format top "- ~(~S~) " (second x))
-                 (format mid "~%### ~(~S~)~%~%`~(~S~)` _(~a)_ ~a~%" 
-                         (second x)
-                         `(,(second x) ,@(third x))
-                         (subseq (symbol-name (first x)) 2)
-                         (fourth x))))))))
+; vim: noai ts=2 sw=2 et:
 
-(let (x)
-  (format t "~%~a~%" 
-    (with-output-to-string (mid)
-      (format t "~%~a~%"  
-        (with-output-to-string (top) 
-          (loop 
-            while (setf x (read-preserving-whitespace t nil))
-            do (worker mid top x)))))))
+"## SYNOPSIS
+
+Generate Markdown from LISP doc strings.
+
+## USAGE
+
+      cat file.lisp      | sbcl --script docs.lisp
+      e.g. cat docs.lisp | sbcl --scropt docs.lisp
+
+## LICENSE
+
+(c) 2020, MIT License, Tim Menzies, timm@ieee.org"
+
+(defun main()
+  "Reading from standard input, for each readable thing, 
+  show the strings (verbatim). Also,
+  for defuns, defmacros, defmethods for doco strings, show their
+  (a)~name, (b)~signature, (c)~doc string as well as (d)~write
+  the name to a table of contents header.  "
+  (let (x
+         (fmttoc "~%~%<br clear=all>~%~%--------~%~%<img width=100 align=right 
+                  src=\"https://sdl-stickershop.line.naver.jp/products/0/0/1/1299646/android/stickers/12120443.png;compress=true\g\">~%~%## ~(~S~)~%~%")
+         (fmtbody "~%### ~(~S~)~%~% `~(~S~)` _(~a)_ ~a~%"))
+    (format t "~%~a~%" 
+      (with-output-to-string (mid)
+        (format t "~%~a~%"  
+          (with-output-to-string (top) 
+            (format top "<img src=\"https://www.lisperati.com/lisplogo_alien_256.png\" align=right>~%~%")
+            (loop while (setf x (read-preserving-whitespace t nil)) do
+              (typecase x
+                (string (format mid "~%~a~%" x))
+                (cons   (case (first x)
+                          (defklass 
+                            (format top "- [~(~S~)](#~(~S~))~%"  
+                                    (second x) (second x))
+                            (format mid fmttoc (second x)))
+                          ((defun defmethod defmacro)
+                           (when (stringp (fourth x))
+                             (format top "  - [~(~S~)](#~(~S~))~%" 
+                                     (second x) (second x))
+                             (format mid fmtbody
+                                     (second x)
+                                     `(,(second x) ,@(third x))
+                                     (case (first x)
+                                       (defun 'FUN)
+                                       (defmacro 'MACRO)
+                                       (defmethod 'METHOD))
+                                     (fourth x))))))))))))))
+
+(main)
